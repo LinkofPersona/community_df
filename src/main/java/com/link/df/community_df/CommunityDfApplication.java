ac57e0717a479197536cee5036f05daf595cb741
link <1607232564@qq.com>
2023-02-17 20:19:48 +0800

    init repo

---
+++ src/main/java/com/link/df/community_df/CommunityDfApplication.java
@@ -0,0 +1,13 @@
+package com.link.df.community_df;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityDfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityDfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/link/df/community_df/CommunityDfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.link.df.community_df;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityDfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
